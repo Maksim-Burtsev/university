7da459f9641c604c6b3dc978de018865eb7f4f47
MaximBurtsev <zadrot-lol@list.ru>
2022-03-04 21:34:47 +0300

    start seven_task

---
+++ seven_task/main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+def index(request):
+
+    if request.method == 'POST':
+        request.session['city'] = request.POST.get('city')
+        request.session['age'] = request.POST.get('age')
+        session = True
+
+    if request.session.get('city'):    
+        context = {
+            'city' : request.session['city'],
+            'age' : request.session['age'],
+            'session' : True,
+        } 
+
+    else:
+        context = {
+            'session' : False,
+        } 
+
+    return render(request, 'main/index.html', context=context)
